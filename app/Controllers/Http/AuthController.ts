2eba83921914fe8eb012c701b82164571deea67c
Daniel Nguyen <anhnguyen.tb90@gmail.com>
2022-05-05 15:15:22 +0700

    Push all

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,38 @@
+import User from "App/Models/User";
+import { schema, rules } from "@ioc:Adonis/Core/Validator";
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+    public async register({ request }: HttpContextContract, response, auth) {
+        /**
+         * Validate user details
+         */
+        const validationSchema = schema.create({
+            email: schema.string({ trim: true }, [
+                rules.email(),
+                rules.unique({ table: "users", column: "email" }),
+            ]),
+            password: schema.string({ trim: true }, [rules.confirmed()]),
+        });
+        const userDetails = await request.validate({
+            schema: validationSchema,
+        });
+        /**
+         * Create a new user
+         */
+        const user = new User();
+        user.email = userDetails.email;
+        user.password = userDetails.password;
+        await user.save();
+        await auth.login(user);
+        response.redirect("/admin/dashboard");
+    }
+
+    public async login({ auth, request, response }: HttpContextContract) {
+        const email = request.input("email");
+        const password = request.input("password");
+        await auth.attempt(email, password);
+        response.redirect("/admin/dashboard");
+    }
+}
